b03e9048fe4679f751bf83f58cf8b56328d2918e
AndreyF1 <andreykatkov13@gmail.com>
2017-12-07 22:58:16 +0300

    testing-2

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+echo "<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    <title>Приветствуем на mytestsite.ru!</title>
+</head>
+
+<body>
+    <h1>Виртуальный хостинг работает!</h1>
+</body>";
+
+?>
